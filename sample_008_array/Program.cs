82f851e0e6c43db23a24bfa4981b916ebfe1d42e
re91na <akhmadeevarr@gmail.com>
2022-10-01 18:31:51 +0500

    sample_008_magic

---
+++ sample_008_array/Program.cs
@@ -0,0 +1,10 @@
+﻿//              0  1  2  3  4 5  6  7  8
+int[] array = {11,61,31,41,15,61,17,18,19};
+int count = 0;
+int max = array[0];
+while(count < array.Length)
+{
+    if(array[count] > max) max = array[count];
+    count ++;
+}
+ Console.WriteLine(max);
