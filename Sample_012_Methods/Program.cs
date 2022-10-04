2777e87debbd76dbcaafdcac9e71ba4d4417dfd9
re91na <akhmadeevarr@gmail.com>
2022-10-04 23:03:51 +0500

    sample_012

---
+++ Sample_012_Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i< array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
